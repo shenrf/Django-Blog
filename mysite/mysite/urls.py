306255719129a9ccda244f262d0fcc2aac3952a4
grewwc <mamawwc@gmail.com>
2017-04-18 19:40:14 +0800

    现在只有两个url能用，一个是admin，另一个是content。blog只有三个属性：title，time和body。在admin里面可以添加blog，然后在content中列出所有的blog的所有属性，即title，time和body。database的属性在settings.py中。

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from blog.views import process_index
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^content/$', process_index),
 ]
